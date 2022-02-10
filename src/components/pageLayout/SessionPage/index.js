dab2ae36cd52702392b38d560ab0078cbe8180e8
alistairlcc <alistairmcclymont@gmail.com>
2022-02-10 19:07:11 +0000

    fix: text and sizing

--- src/components/pageLayout/SessionPage/index.js
+++ src/components/pageLayout/SessionPage/index.js
@@ -45,13 +45,8 @@ const SessionPage = ({ sessionPages }) => {
       </div>
       <section className={styles.introduction}>
         <p>
-          From A to B, left to right, corner to plane, on screen to off screen;
-          things require movement and mechanism, from one position to another.
-          GMD lecturers Deborah and Alistair in their practice often use the
-          observation of phenomena, and the creation of assemblage(s) in their
-          work. These practice-based sessions invite you to be part of their
-          studio work, experimenting with methods of cause and effect and chain
-          reactions.
+          Each week <em>Assemblage</em> focuses on a different area, moving
+          towards the final chain reaction.
         </p>
       </section>
 
